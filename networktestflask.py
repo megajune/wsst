03c8d908a6dac50536a66f161a19f916ef8c357c
martinj <martinj@mailbox.sc.edu>
2022-09-02 15:13:54 -0400

    Notes

--- networktestflask.py
+++ networktestflask.py
@@ -16,9 +16,15 @@ def contest():
         #Attempt to access the target web server.
         t2 = requests.get(request.args['proto']+"://"+request.args['desthost']+":"+request.args['destport'])
     except Exception:
-        error = "DNS could not resolve " + request.args['desthost'] + ", there is a problem in your local network, or the web server is down."   
+        error = []
+        if t2.status_code not in [200,301,302]:
+            error.append("This application was unable to connect to " + t2 + " with a HTTP connection.")
+        if t1 == "":
+            error.append(" DNS could not resolve" + request.args['desthost'])
+        #error = "DNS could not resolve " + request.args['desthost'] + ", there is a problem in your local network, or the web server is down."   
     #If the error variable is set, it will be in the "locals" variable
-    if 'error' not in locals():
+    #if 'error' not in locals():
+    if error.count == 0:
         #If the locals() method confirms the error variable was never defined, do the following:
         response = make_response(
                 jsonify(
@@ -30,7 +36,7 @@ def contest():
         #If error was defined, return a negative error status with a http 418
         response = make_response(
                 jsonify(
-                    {"servername": request.args['desthost'], "message": request.args['desthost'] + " returned this error - " + error}
+                    {"servername": request.args['desthost'], "message": request.args['desthost'] + error}
                 ),
                 #Return an HTTP 418 if an error was detected
                 418,
@@ -40,4 +46,4 @@ def contest():
 
 if __name__ == "__main__":
      # Launch the Flask dev server
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", ssl_context='adhoc')
